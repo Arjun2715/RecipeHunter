8019c08e0e827b4e006ca48eaed86e61ec091744
Arjun2715 <arjunjot18@gmail.com>
2023-03-27 10:44:15 +0200

    vue base

--- resources/js/Layouts/RecipeHunterLayout.vue
+++ resources/js/Layouts/RecipeHunterLayout.vue
@@ -1,25 +1,25 @@
-<script setup></script>
+
+<script setup>
+import RecipeHunterLayout from "../Layouts/RecipeHunterLayout.vue";
+  defineProps({
+    articles: Object,
+  });
+</script>
 
 <template>
-  <Header
-      class="bg-gradient-to-r from-blue-700 via-indigo-700 to-blue-700 w-full text-center py-4"
-  >
-      <h1 class="text-white font-bold text-4xl">Kinsta Blog</h1>
+  <RecipeHunterLayout>
+
+  <Header>
+     
   </Header>
 
-  <main class="container mx-auto text-center">
-      <slot />
+  <main>
+
   </main>
 
-  <footer
-      class="bg-gradient-to-b from-transparent to-gray-300 w-full text-center mt-5 py-10 mx-auto"
-  >
-      <h2 class="font-bold text-xl pb-5">Join our Newsletter</h2>
+  <footer>
 
-      <input
-          class="rounded-xl w-80 h-12 px-3 py-2 shadow-md"
-          type="email"
-          placeholder="Write your email.."
-      />
   </footer>
+      
+</RecipeHunterLayout>
 </template>
