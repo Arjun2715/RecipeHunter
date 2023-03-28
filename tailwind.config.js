3a6aa4f253e7f490970c54aafbec5a955bcfcf21
Arjun2715 <arjunjot18@gmail.com>
2023-03-28 13:30:23 +0200

    search

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lemon' : '#CAE00D',
+        'lemon-20' : 'rgba(202, 224, 13, 0.2);',
+        'lemon-40' : 'rgba(202, 224, 13, 0.4);',
+        'lemon-60' : 'rgba(202, 224, 13, 0.6);',
+        'lemon-80' : 'rgba(202, 224, 13, 0.8);',
+
+
+        'green' : '#00FF92',
+        'green-20' : 'rgba(0, 255, 146, 0.2)',
+        'green-40' : 'rgba(0, 255, 146, 0.4)',
+        'green-60' : 'rgba(0, 255, 146, 0.6)',
+        'green-80' : 'rgba(0, 255, 146, 0.8)',
+      }
+    },
   },
   plugins: [],
   plugins: [require("daisyui")],
