ff7b45b8454a3f001591326106916078bd87c5a6
Arjun2715 <arjunjot18@gmail.com>
2023-03-31 10:17:34 +0200

    home nav setup

--- resources/js/Layouts/RecipeHunterLayout.vue
+++ resources/js/Layouts/RecipeHunterLayout.vue
@@ -17,9 +17,10 @@ defineProps({
 
 <template >
     <body class="">
-        <Header>
-            <div class="navbar bg-base-100">
-                <div class="navbar-start">
+        <Header class="mt-4">
+
+            <div class=" flex container mx-auto px-4 h-full">
+                <div class="navbar-start flex">
                     <!--mobile-->
                     <div class="dropdown">
                         <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -44,7 +45,7 @@ defineProps({
 
                 </div>
                 <!--windowed-->
-                <div class="navbar-center hidden lg:flex">
+                <div class="navbar-center hidden lg:flex px-4">
                     <ul class="menu menu-horizontal px-1">
                         <li>
                             <Link href="#">Home</Link>
@@ -66,15 +67,16 @@ defineProps({
                         </li>
                     </ul>
                 </div>
-                <div class="navbar-end  ">
+                <div class="navbar-end  flex px-4">
                     <div class="form-control hidden lg:flex mr-2">
                         <!-- <button class="border border-2 bg-gradient-to-r from-blue-400 to-green-400 text-white font-bold py-2 px-4 rounded-full">Click me</button> -->
-                        <input type="text" placeholder="Search" class="input input-bordered border-2 input-accent bg-gradient-to-r from-lemon-20 to-green-20 rounded-full" />
+                        <input type="text" placeholder="Search"
+                            class="input input-bordered border-2  input-accent bg-gradient-to-r from-lemon-20 to-green-20 rounded-full" />
                     </div>
                     <div class="dropdown dropdown-end">
                         <label tabindex="0" class="btn btn-ghost btn-circle avatar ">
-                            <div class="w-10 rounded-full">
-                                <img class="w-10 h-10 rounded-full" src="assets/images/avatar.jpg" alt="">
+                            <div class="w-10 rounded-full ">
+                                <img class="w-10 h-10 rounded-full  " src="assets/images/avatar.jpg" alt="">
 
                             </div>
                         </label>
@@ -115,22 +117,22 @@ defineProps({
                         <path
                             d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z">
                         </path>
-                        </svg>
-                    </a>
-                    <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            class="fill-current">
-                            <path
-                                d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z">
-                            </path>
-                        </svg></a>
-                    <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            class="fill-current">
-                            <path
-                                d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z">
-                            </path>
-                        </svg></a>
+                            </svg>
+                        </a>
+                        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                class="fill-current">
+                                <path
+                                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z">
+                                </path>
+                            </svg></a>
+                        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                class="fill-current">
+                                <path
+                                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z">
+                                </path>
+                            </svg></a>
+                    </div>
                 </div>
-            </div>
-        </footer> -->
+            </footer> -->
     </body>
 </template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<script scope>
+import navbar from "../Layouts/components/Navbar.vue";
+import RecipeHunterLayout from "../Layouts/RecipeHunterLayout.vue";
+export default {
+    components: {
+    RecipeHunterLayout
+}
+}
+</script>
+<template>
+    <main>
+        <section class="absolute w-full h-xl bg-lemon-20">
+            <RecipeHunterLayout />
+            <div class="w-full h-full bg-green-20">
+                
+            </div>
+        </section>
+    </main>
+</template>
