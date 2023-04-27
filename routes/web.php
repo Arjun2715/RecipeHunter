b7f2fcd2f82967fff472e736d9980b6fe1ad4593
joanet501 <joanvidalmezquita@gmail.com>
2023-04-27 17:05:34 +0200

    get categories endpoint created

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Inertia\Inertia;
 Route::get('/home', [WebController::class,'home'])->name('home');
 Route::get('/categories', [WebController::class,'categories'])->name('categories');
 
+Route::get('/categories/{category_id}', [WebController::class,'getCategory'])->name('getCategory');
+
+
 Route::get('/filter', [WebController::class,'searchRand'])->name('filter');
 Route::post('/filter', [WebController::class,'searchRecipes'])->name('filtersearch');
 
