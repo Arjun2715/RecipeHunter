aa6008cc638233a8d657ec3fe3aef07cbbbbe7e4
Arjun2715 <arjunjot18@gmail.com>
2023-05-03 12:36:55 +0200

    -_-

--- resources/js/Pages/ViewRecipe.vue
+++ resources/js/Pages/ViewRecipe.vue
@@ -100,13 +100,13 @@ export default {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="rating rating-md align-bottom">
+                                <!-- <div class="rating rating-md align-bottom">
                                     <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" />
                                     <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" />
                                     <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" />
                                     <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" checked />
                                     <input type="radio" name="rating-7" class="mask mask-star-2 bg-orange-400" />
-                                </div>
+                                </div> -->
                                 <label class="text-gray-700 dark:text-white">{{ processedDescription }}
                                 </label>
                                 <div v-if="this.data.recipe.data.categories != 0"
